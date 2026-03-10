94b9bc935d09f57b5532536eb8f3642371817202
booscaaa <boscardinvinicius@gmail.com>
2026-03-10 14:07:01 -0300

    :sparkles: feat(web/vite.config): filter non-woff2 font preloads in output

--- web/vite.config.mjs
+++ web/vite.config.mjs
@@ -10,6 +10,18 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
+function filterFontPreloads() {
+  return {
+    name: 'filter-font-preloads',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<link rel="preload" as="font" type="font\/(?!woff2)[^"]*"[^>]*>\n?/g,
+        ''
+      )
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -30,6 +42,7 @@ export default defineConfig({
         ],
       },
     }),
+    filterFontPreloads(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'looksmith.png'],
