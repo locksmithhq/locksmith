8d1042537f73d1d16342858eef028664a85f6d42
booscaaa <boscardinvinicius@gmail.com>
2026-03-10 00:05:03 -0300

    :card_file_box: feat(db): create migration to add favicon_url to oauth clients

---
+++ config/database/migrations/000020_add_favicon_url_to_login_signup.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE oauth_clients_login DROP COLUMN favicon_url;
+ALTER TABLE oauth_clients_signup DROP COLUMN favicon_url;
---
+++ config/database/migrations/000020_add_favicon_url_to_login_signup.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE oauth_clients_login ADD COLUMN favicon_url TEXT;
+ALTER TABLE oauth_clients_signup ADD COLUMN favicon_url TEXT;
