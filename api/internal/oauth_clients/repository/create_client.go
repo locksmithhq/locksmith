4944cd908a2e8aeac1e22b9133141be4f3c73ec9
booscaaa <boscardinvinicius@gmail.com>
2026-04-23 17:00:43 -0300

    ✨ feat(create_client): adds require_pkce to INSERT and encrypts client_secret at write

--- api/internal/oauth_clients/repository/create_client.go
+++ api/internal/oauth_clients/repository/create_client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/booscaaa/initializers/postgres/types"
+	"github.com/locksmithhq/locksmith/api/internal/core/crypto"
 	"github.com/locksmithhq/locksmith/api/internal/core/types/stackerror"
 	"github.com/locksmithhq/locksmith/api/internal/oauth_clients/contract"
 	"github.com/locksmithhq/locksmith/api/internal/oauth_clients/domain"
@@ -19,21 +20,32 @@ func (r *createClientRepository) Execute(ctx context.Context, input input.Client
 	var client domain.Client
 
 	query := `INSERT INTO oauth_clients (
-		project_id, name, client_id, client_secret, redirect_uris, grant_types, custom_domain
-	) VALUES ($1, $2, $3, $4, $5, $6, $7) 
-	RETURNING id, project_id, client_id, client_secret, redirect_uris, grant_types, name, created_at, updated_at, custom_domain`
+		project_id, name, client_id, client_secret, redirect_uris, grant_types, custom_domain, require_pkce
+	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+	RETURNING id, project_id, client_id, client_secret, redirect_uris, grant_types, name, created_at, updated_at, custom_domain, require_pkce`
 
-	err := r.database.QueryRowxContext(ctx,
+	encryptedSecret, err := crypto.Encrypt(input.ClientSecret)
+	if err != nil {
+		return domain.Client{}, stackerror.NewRepositoryError("CreateClientRepository", err)
+	}
+
+	err = r.database.QueryRowxContext(ctx,
 		query,
 		input.ProjectID,
 		input.Name,
 		input.ClientID,
-		input.ClientSecret,
+		encryptedSecret,
 		input.RedirectURIs,
 		input.GrantTypes,
 		input.CustomDomain,
+		input.RequirePKCE,
 	).StructScan(&client)
 
+	if err != nil {
+		return domain.Client{}, stackerror.NewRepositoryError("CreateClientRepository", err)
+	}
+
+	client.ClientSecret, err = crypto.Decrypt(client.ClientSecret)
 	if err != nil {
 		return domain.Client{}, stackerror.NewRepositoryError("CreateClientRepository", err)
 	}
