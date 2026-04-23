3d4f7133d25d52e7a8abdd9b35cd3d541ca015f0
booscaaa <boscardinvinicius@gmail.com>
2026-04-23 17:04:03 -0300

    ✨ feat(logout_handler): wires logout handler with LogoutUseCase

--- api/internal/locksmith/di/logout_handler.go
+++ api/internal/locksmith/di/logout_handler.go
@@ -3,8 +3,11 @@ package di
 import (
 	"github.com/locksmithhq/locksmith/api/internal/locksmith/contract"
 	"github.com/locksmithhq/locksmith/api/internal/locksmith/handler"
+	oauth2Di "github.com/locksmithhq/locksmith/api/internal/oauth2/di"
 )
 
 func NewLogoutHandler() contract.LogoutHandler {
-	return handler.NewLogoutHandler()
+	return handler.NewLogoutHandler(
+		oauth2Di.NewLogoutUseCase(),
+	)
 }
