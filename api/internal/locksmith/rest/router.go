fc13a6505ccebde645829df5da179585fe7c933d
booscaaa <boscardinvinicius@gmail.com>
2026-04-23 17:04:00 -0300

    ✨ feat(locksmith/rest/router): adds per-IP rate limiting on logout and refresh endpoints

--- api/internal/locksmith/rest/router.go
+++ api/internal/locksmith/rest/router.go
@@ -1,13 +1,17 @@
 package rest
 
 import (
+	"time"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httprate"
 	"github.com/locksmithhq/locksmith/api/internal/locksmith/di"
 )
 
 func InitializeLocksmithRouter(router chi.Router) {
 	router.Get("/locksmith/callback", di.NewCallbackHandler().Execute)
-	router.Post("/locksmith/logout", di.NewLogoutHandler().Execute)
 	router.Get("/locksmith/status", di.NewStatusHandler().Execute)
-	router.Post("/locksmith/r", di.NewRefreshTokenHandler().Execute)
+
+	router.With(httprate.LimitByIP(20, time.Minute)).Post("/locksmith/logout", di.NewLogoutHandler().Execute)
+	router.With(httprate.LimitByIP(20, time.Minute)).Post("/locksmith/r", di.NewRefreshTokenHandler().Execute)
 }
