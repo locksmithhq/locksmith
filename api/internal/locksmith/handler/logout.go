1806d6c7f32602faaeedfebdb2283c405bbbc6c5
booscaaa <boscardinvinicius@gmail.com>
2026-04-23 17:03:57 -0300

    ✨ feat(logout): revokes refresh token and session before clearing cookies

--- api/internal/locksmith/handler/logout.go
+++ api/internal/locksmith/handler/logout.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 
 	"github.com/locksmithhq/locksmith/api/internal/locksmith/contract"
+	oauth2Contract "github.com/locksmithhq/locksmith/api/internal/oauth2/contract"
 )
 
-type logoutHandler struct{}
+type logoutHandler struct {
+	logoutUseCase oauth2Contract.LogoutUseCase
+}
 
 // Execute implements contract.LogoutHandler.
 func (h *logoutHandler) Execute(w http.ResponseWriter, r *http.Request) {
+	if cookie, err := r.Cookie("LOCKSMITHREFRESHTOKEN"); err == nil && cookie.Value != "" {
+		_ = h.logoutUseCase.Execute(r.Context(), cookie.Value)
+	}
+
 	domain := getDomainFromRequest(r)
 	http.SetCookie(w, &http.Cookie{
 		Name:     "LOCKSMITHACCESSTOKEN",
@@ -34,6 +41,6 @@ func (h *logoutHandler) Execute(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func NewLogoutHandler() contract.LogoutHandler {
-	return &logoutHandler{}
+func NewLogoutHandler(logoutUseCase oauth2Contract.LogoutUseCase) contract.LogoutHandler {
+	return &logoutHandler{logoutUseCase: logoutUseCase}
 }
